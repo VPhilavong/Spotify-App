862290f11295c7a843ccf61f4d41149d7b766e62
150jason <143555002+150jason@users.noreply.github.com>
2024-05-16 02:19:40 +0000

    finished the artist search function, it works time to start coding for real

--- Spotify App/auth.py
+++ Spotify App/auth.py
@@ -36,8 +36,29 @@ def search_for_artist(token, artist_name):
 
     query_url = url + query
     result = get(query_url, headers=headers)
-    json_result = json.loads(result.content)
-    print(json_result)
+    json_result = json.loads(result.content)["artists"]["items"]
+    
+    if len(json_result) == 0:
+        print("This dude dont exist")
+        return None
+    
+    return json_result[0]
+  
 
+def get_songs_by_artist(token, artist_id):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)["tracks"]
+    return json_result
 token = get_token()
-search_for_artist(token, "Drake")
+artist_result = search_for_artist(token, "Playboi Carti")
+if artist_result:
+    print(artist_result["name"])
+artist_id = artist_result["id"]
+songs = get_songs_by_artist(token, artist_id)
+print(songs)
+
+
+for idx, song in enumerate(songs):
+    print(f"{idx + 1}. {song['name']}")
