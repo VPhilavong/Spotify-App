ee4ba36e8d78a8fcb0f6647486997f88f4c11d59
VPhilavong <125309983+VPhilavong@users.noreply.github.com>
2024-05-23 07:04:41 +0000

    setup react

--- Spotiapp/app/models.py
+++ Spotiapp/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    albums = models.IntegerField()
+    tracks = models.IntegerField()
+    self = models.CharField(max_length=100)
