c56a11cc50408c27314fc57ca1fae2ee2857c006
Vincent <vphilavong0@gmail.com>
2024-09-09 01:59:47 -0500

    functional for different users

--- Spotiapp/dashboard/urls.py
+++ Spotiapp/dashboard/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
+from spotify.views import auth_url, spotify_callback, logout
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('spotify/login/', views.spotify_login, name='spotify_login'),
-    path('spotify/callback/', views.spotify_callback, name='spotify_callback'),
-    path('logout/', views.logout, name='logout'),
+    path('spotify/login/', auth_url, name='spotify_login'),
+    path('spotify/callback/', spotify_callback, name='spotify_callback'),
+    path('spotify/logout/', logout, name='spotify_logout'),
     path('top_artists/', views.top_artists, name='top_artists'),
-    path('top_genres/', views.top_genres, name='top_genres'),
     path('top_tracks/', views.top_tracks, name='top_tracks'),
+    path('top_genres/', views.top_genres, name='top_genres'),
+    path('recently_played/', views.recently_played, name='recently_played'),
+    path('test/', views.test, name='test'),
     path('', views.login, name='login'),
 ]
